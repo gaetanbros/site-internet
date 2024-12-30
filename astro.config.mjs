ac97e99f68c90c56f373e053379be47fe42a3b22
AlessandroMA <marellalessandro@gmail.com>
2024-12-30 19:02:04 +0100

    fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // Import the Vercel adapter
-import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gaetan-bros.github.io",
+  site: "https://alessandroma.github.io",
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel(),
 });
