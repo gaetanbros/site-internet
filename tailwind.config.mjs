7ccd998eddb895ed641aa028eaa452449ddf9647
AlessandroMA <marellalessandro@gmail.com>
2024-12-30 17:54:24 +0100

    minor fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,6 +12,7 @@ export default {
     },
     extend: {
       animation: {
+        "spin-slow": "spin 5s linear infinite",
         "infinite-scroll": "infinite-scroll 25s linear infinite",
         "reverse-infinite-scroll":
           "reverse-infinite-scroll 25s linear infinite",
