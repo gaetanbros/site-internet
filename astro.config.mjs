567ac42c787dc0904b582a564ddbe98edb994a8c
AlessandroMA <marellalessandro@gmail.com>
2024-12-31 10:53:58 +0100

    clean vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-// Import the Vercel adapter
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://alessandroma.github.io",
