5ca118799bfacca7c9955d4b75661b44cc6bd071
AlessandroMA <marellalessandro@gmail.com>
2025-01-08 12:30:46 +0100

    feat: mobile responsiveness

--- src/components/react/Slider.jsx
+++ src/components/react/Slider.jsx
@@ -5,33 +5,51 @@ import e1 from "/src/assets/elements/element1.png";
  */
 import { AnimatePresence, motion } from "motion/react";
 import { useState } from "react";
-
+const successStories = [
+  {
+    title: "Veermer, la lumière du silence",
+    summary:
+      "Pourquoi une simple femme affublée d'une perle ou une laitière peuvent-elles nous émouvoir si fortement trois siècles après leur réalisation ? Comment un tel génie a-t-il pu être oublié pendant près de deux siècles ? Parcourez l’âge d’or hollandais, et découvrez les secrets d’un Vermeer dans l’intimité de ses peintures, qui n’ont pas fini de nous émerveiller.",
+    image: e3,
+  },
+  {
+    title: "Les mystérieuses citées d’or : Les trésors du Pérou",
+    summary:
+      "Immenses glyphes dont les figures ne sont visibles que du ciel, divinités étranges et colorés sur des pots de terres, cités désertes, tombes comblent d’or, tous ces vestiges sont autant de signes de la grandeur des civilisations Péruviennes. Venez décrypter ces indices magnifiques et mystérieux, plongez dans la création des ancêtres des Incas.",
+    image: e2,
+  },
+  {
+    title: "Eros et Psyché : la plus belle des histoires d’amour",
+    summary:
+      "Eros, enfant d’Aphrodite, embrase le cœur de ceux qu’il touche avec ses flèches. Les mythes grecs en font parfois un dieu fondamental de la création, et d’autres fois l’initiateur du désordre passionnel. Ce démon ambigu du désir n’eut qu’un amour et pas des moindres puisque c’est avec Psyché, la plus belle des humaines, dont le nom signifie en grec l’âme.",
+    image: e1,
+  },
+];
+const Image = ({ activeIndex }) => (
+  <AnimatePresence mode="wait">
+    <motion.div
+      key={activeIndex}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+    >
+      <img
+        src={successStories[activeIndex]?.image.src}
+        alt="Star Shape"
+        className="motion-scale-y-loop-105 motion-scale-x-loop-105 motion-duration-2000 max-w-[10rem] w-[10rem] lg:max-w-[20rem] lg:w-[20rem]"
+      />
+    </motion.div>
+  </AnimatePresence>
+);
 export function Slider() {
   const [activeIndex, setActiveIndex] = useState(0);
-  const successStories = [
-    {
-      title: "Veermer, la lumière du silence",
-      summary:
-        "Pourquoi une simple femme affublée d'une perle ou une laitière peuvent-elles nous émouvoir si fortement trois siècles après leur réalisation ? Comment un tel génie a-t-il pu être oublié pendant près de deux siècles ? Parcourez l’âge d’or hollandais, et découvrez les secrets d’un Vermeer dans l’intimité de ses peintures, qui n’ont pas fini de nous émerveiller.",
-      image: e3,
-    },
-    {
-      title: "Les mystérieuses citées d’or : Les trésors du Pérou",
-      summary:
-        "Immenses glyphes dont les figures ne sont visibles que du ciel, divinités étranges et colorés sur des pots de terres, cités désertes, tombes comblent d’or, tous ces vestiges sont autant de signes de la grandeur des civilisations Péruviennes. Venez décrypter ces indices magnifiques et mystérieux, plongez dans la création des ancêtres des Incas.",
-      image: e2,
-    },
-    {
-      title: "Eros et Psyché : la plus belle des histoires d’amour",
-      summary:
-        "Eros, enfant d’Aphrodite, embrase le cœur de ceux qu’il touche avec ses flèches. Les mythes grecs en font parfois un dieu fondamental de la création, et d’autres fois l’initiateur du désordre passionnel. Ce démon ambigu du désir n’eut qu’un amour et pas des moindres puisque c’est avec Psyché, la plus belle des humaines, dont le nom signifie en grec l’âme.",
-      image: e1,
-    },
-  ];
+
   return (
-    <div className="flex flex-row justify-between rounded-[45px] py-10 bg-white">
-      <div className="w-1/2 pr-10 relative">
-        <div className="bg-green w-full h-full absolute z-[-1]" />
+    <div className="flex flex-col lg:flex-row justify-between rounded-[45px] lg:py-10 bg-white">
+      <div className="lg:hidden flex flex-row  justify-center items-center rounded-[45px] ">
+        <Image activeIndex={activeIndex} />
+      </div>
+      <div className="pt-4 lg:pt-0 flex-row lg:flex-col lg:w-1/2 lg:pr-10 relative">
         {successStories.map((story, index) => (
           <div
             key={index + "tes"}
@@ -57,22 +75,8 @@ export function Slider() {
           </div>
         ))}
       </div>
-      <div className="w-1/2 flex justify-center items-center bg-purple  shadow-2xl rounded-l-[45px]">
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={activeIndex}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5, ease: "easeInOut" }}
-          >
-            {" "}
-            <img
-              src={successStories[activeIndex].image.src}
-              alt="Star Shape"
-              className="motion-scale-y-loop-105 motion-scale-x-loop-105 motion-duration-2000 max-w-[15rem] w-[15rem] lg:max-w-[20rem] lg:w-[20rem]"
-            />
-          </motion.div>
-        </AnimatePresence>
+      <div className="hidden flex-row lg:flex-col lg:w-1/2 lg:flex justify-center items-center bg-purple  shadow-2xl rounded-l-[45px]">
+        <Image activeIndex={activeIndex} />
       </div>
     </div>
   );
