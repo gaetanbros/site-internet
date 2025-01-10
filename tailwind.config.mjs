e5dea999e6d53b590f99b8b8f64a2878f2d5b628
AlessandroMA <marellalessandro@gmail.com>
2025-01-10 18:39:55 +0100

    feat: update visuals

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -18,11 +18,35 @@ export default {
         section: "5px 5px 41px 5px #2e2c2c",
       },
       animation: {
+        blink: "blink 4s  cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite",
+        blink2: "blink2 4s  cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite",
         "infinite-scroll": "infinite-scroll 30s linear infinite",
         "reverse-infinite-scroll":
           "reverse-infinite-scroll 25s linear infinite",
       },
       keyframes: {
+        blink: {
+          "0%": {
+            "clip-path": "ellipse(36% 6% at 50% 40%)",
+          },
+          "12%": {
+            "clip-path": "ellipse(36% 0% at 50% 40%)",
+          },
+          "14%, 100%": {
+            "clip-path": "ellipse(36% 6% at 50% 40%)",
+          },
+        },
+        blink2: {
+          "0%": {
+            "clip-path": "ellipse(36% 6% at 50% 22%)",
+          },
+          "12%": {
+            "clip-path": "ellipse(36% 0% at 50% 22%)",
+          },
+          "14%, 100%": {
+            "clip-path": "ellipse(36% 6% at 50% 22%)",
+          },
+        },
         "infinite-scroll": {
           from: { transform: "translateX(0)" },
           to: { transform: "translateX(-100%)" },
