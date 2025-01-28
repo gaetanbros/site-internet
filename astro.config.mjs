89747a2f27b59211ee7e5ed97ad1504e0dc29e46
AlessandroMA <marellalessandro@gmail.com>
2025-01-28 18:00:13 +0100

    fix: astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alessandroma.github.io",
-  integrations: [tailwind(), react()],
+  site: "https://gaetanbros.fr",
+  integrations: [tailwind(), react(), sitemap()],
   /*   server: {
     port: 4321,
     host: "192.168.1.15",
